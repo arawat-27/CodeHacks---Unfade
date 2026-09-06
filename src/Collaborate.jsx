92805dfb2f973ac0054bfe1a3c2445ceb9b91f19
saideekshithaganji <sganji@calpoly.edu>
2026-09-06 11:26:29 -0700

    Fix Collaborate page component

--- src/Collaborate.jsx
+++ src/Collaborate.jsx
@@ -139,16 +139,3 @@ export default function Collaborate() {
     </>
   );
 }
-      <main className="page-content">
-        <p className="page-label">WORK TOGETHER</p>
-        <h1>Collaborate</h1>
-        <p>
-          Find other students who are looking for artists, actors, musicians,
-          writers, designers, and more.
-        </p>
-      </main>
-    </div>
-  );
-}
-
-export default Collaborate;
